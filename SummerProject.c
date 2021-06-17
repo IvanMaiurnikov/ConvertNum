2acc6f614b01fe8f4c522734ce0b8358ca548846
Ivan Maiurnikov <vanchos.majos@gmail.com>
2021-06-17 16:56:18 +0300

    Moved along in writing a program. Finally have added conversion function although they are still undergoing maintanance

--- SummerProject.c
+++ SummerProject.c
@@ -4,29 +4,106 @@
 
 #define MAX_LEN (12)
 
-char* SIGNLE_DIGIT[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+char* SINGLE_DIGIT[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-char* DOUBLE_DIGIT[] = {"ten", "eleven", "twelth", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+char* TENTH_DIGIT[] = {"ten", "eleven", "twelth", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
-char* MULT[] = {"hundred", "thousand", "million", "billion"};
+char* DOUBLE_DIGIT[] = { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+char* MULT[] = {"", "ten", "hundred", "thousand", "million", "billion"};
 
 char* SIGN[] = {"", "minus"};
 
-void conversion(sds in, sds* out, int val) {
-	if (val < 0) {
-		out = sdscat(*out, "minus");
-	}
-	if (val / 1000000000 > 0) {
-		out = sdscat(*out, "billion");
+int trio_translate(sds raw, sds* processed) {
+	int i,
+		rc = 0,
+		len,
+		adr;
+	len = sdslen(raw);
+	// test the values, change between sizes!!!!
+		if (len > 2) {
+			adr = raw[0] - '0';
+			if (raw[0] != '0') {
+				sdscat(*processed, SINGLE_DIGIT[adr]);
+				sdscat(*processed, MULT[1]);
+			}
+			adr = raw[1] - '0';
+				if (raw[1] == '1') {
+					adr = raw[2] - '0';
+					sdscat(*processed, TENTH_DIGIT[adr]);
+				}
+				if (raw[1] != 1) {
+					sdscat(*processed, DOUBLE_DIGIT[adr]);
+					adr = raw[2] - '0';
+					sdscat(*processed, SINGLE_DIGIT[adr]);
+				}
+				rc = 1;
+		}
+		if (len < 3) {
+			if (len == 2) {
+				adr = raw[0] - '0';
+				if (raw[0] == '1') {
+					adr = raw[1] - '0';
+					sdscat(*processed, TENTH_DIGIT[adr]);
+				}
+				if (raw[0] != '1') {
+					sdscat(*processed, DOUBLE_DIGIT[adr]);
+					adr = raw[1] - '0';
+					sdscat(*processed, SINGLE_DIGIT[adr]);
+				}
+			}
+			if (len == 1) {
+				adr = raw[0] - '0';
+				sdscat(*processed, SINGLE_DIGIT[adr]);
+			}
+			rc = 1;
+		}
+		return rc;
+}
 
+void conversion(sds in, sds* out) {
+	int i,
+		count,
+		subcount,
+		start = 0,
+		weight,
+		lose,
+		finish = 0;
+	sds* clone,
+		sign_tokens,
+		subtoken;
+	sdstrim(in," \n");
+	if (in[0] == '-') { // check if the number is <0
+		sdstrim(in, "-");
+		sdscat(*out, SIGN[1]);
 	}
+
+		weight = (((strlen(in) / 3) + (strlen(in) % 3 ? 1 : 0))) - 1;
+		do {
+			sds clone = sdsnew(in, sdslen(in));
+			if (strlen(in) % 3 > 0) {
+				lose = (strlen(in)) % 3;
+				finish = (strlen(in) % 3) - 1;
+				sdsrange(in, start, finish);
+				trio_translate(in, &out);
+				start = finish + 1;
+				finish += finish + 3;
+				weight -= weight - lose;
+			}
+			else {
+				finish += 2;
+			}
+			
+			weight = weight - 3;
+		} while (weight); 
+	
 }
 
 	int input_req() {
 		int number;
 		do {
 			scanf("%d", &number);
-			if (number < sizeof(int) || number > sizeof(int)) {
+			if (number < INT_MIN || number > INT_MAX) {
 				printf("Input exceeds 32 bit number. Please try again.");
 				continue;
 			}
@@ -55,7 +132,7 @@ int main()
 	input = input_req(); // make an rc, if int fails send error and retry 
 	sprintf(buff, "%11d", input);
 	sds str = sdsnew(buff, strlen(buff));
-	sds proc = sdsnew("", MAX_LEN); // sending empty string
-	conversion(str, proc, input);
+	sds proc = sdsnew("", 20); // sending empty string
+	conversion(str, &proc);
 }
 
