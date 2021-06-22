90b718ffa5e0df181b0d6e85c9f006849cace52a
Ivan Maiurnikov <vanchos.majos@gmail.com>
2021-06-22 15:34:43 +0300

    Changed project file names

--- ConvertNum.c
+++ ConvertNum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sds.h>
 #include <Windows.h>
+#include <SummerProject.h>
 
 #define MAX_LEN (13)
 
---
+++ ConvertNum.h
@@ -0,0 +1,6 @@
+#ifndef _SUMMER_PROJECT
+#define _SUMMER_PROJECT
+#include <sds.h>
+int trio_translate(sds raw, sds* processed);
+void conversion(sds in, sds* out);
+#endif
